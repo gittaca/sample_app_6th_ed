c211389dde7d69490d6836e5e16c667488a2ddcd
gittaca <gittaca@users.noreply.github.com>
2020-12-28 15:54:52 +0100

    Align equal signs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
     BASE_URL   = 'https://secure.gravatar.com/avatar'
     # Alternative: https://cdn.libravatar.org/avatar
     
-    size         = options[:size]
+    size       = options[:size]
     avatar_id  = Digest::MD5::hexdigest(user.email.downcase)
     avatar_url = "#{BASE_URL}/#{avatar_id}?s=#{size}"
     image_tag(avatar_url, alt: user.name, class: "avatar")
