d44b43097c405dd953d5930ef50a2c04f706ccb6
gittaca <gittaca@users.noreply.github.com>
2020-12-28 15:42:53 +0100

    Generalise avatar-related code

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
 
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
+  # Returns an avatar for the given user.
+  def avatar_for(user, options = { size: 80 })
+    
     size         = options[:size]
-    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    avatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    avatar_url = "#{avatar_id}?s=#{size}"
+    image_tag(avatar_url, alt: user.name, class: "avatar")
   end
 end
