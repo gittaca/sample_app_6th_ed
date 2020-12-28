55f6808ad677ff6d251bd2bc88ab24ac91e5fc79
gittaca <gittaca@users.noreply.github.com>
2020-12-28 15:51:15 +0100

    Extract Gravatar base URL & comment alternative

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,10 +2,12 @@ module UsersHelper
 
   # Returns an avatar for the given user.
   def avatar_for(user, options = { size: 80 })
+    BASE_URL   = 'https://secure.gravatar.com/avatar'
+    # Alternative: https://cdn.libravatar.org/avatar
     
     size         = options[:size]
     avatar_id  = Digest::MD5::hexdigest(user.email.downcase)
-    avatar_url = "#{avatar_id}?s=#{size}"
+    avatar_url = "#{BASE_URL}/#{avatar_id}?s=#{size}"
     image_tag(avatar_url, alt: user.name, class: "avatar")
   end
 end
